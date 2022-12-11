6524317dbda6776acd202aec781b157336f6fc8c
Marlena Tsang <Marlena345@gmail.com>
2022-12-11 11:49:18 -0500

    Modified-changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {DataProvider} from './DataContext';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MenuNav from './components/MenuNav'
+import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm';
+import Home from './components/Home';
+import ErrorPage from './components/ErrorPage'
+import Onboarding from './components/Onboarding';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <MenuNav 
+        />
+        <main>
+          <Routes>
+            <Route 
+              path="/" 
+              element={
+                <Home />
+              } 
+            />
+              <Route 
+              path="/onboarding"
+              element={<Onboarding />}
+              />
+            <Route 
+              path="/signup" 
+              element={
+                <SignupForm />
+              } 
+            />
+            <Route
+            path="/login"
+            element={ <LoginForm />}
+            />
+            
+            <Route 
+              path="*" 
+              element={<ErrorPage />} 
+            />
+          </Routes>
+        </main>
+    </Router>
+  </DataProvider>
   );
 }
 
