256bcc8bf2e71c5cdb5f42be3eaa6a6fcd6c9538
Marlena Tsang <marlena345@gmail.com>
2022-12-11 12:14:50 -0500

    error page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <br />
+      <h1>
+        ERROR 404 PAGE NOT FOUND
+      </h1>
+       <br />
+   </div>
+  )
+}
+
+export default ErrorPage
